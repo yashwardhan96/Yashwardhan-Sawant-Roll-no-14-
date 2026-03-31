1652b83f3b143b4126f42ad1e3e75a68f401db06
yashwardhan96 <yashwardhansawant969696@gmail.com>
2026-03-31 22:27:36 +0530

    Update percentage.cpp

--- percentage.cpp
+++ percentage.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main() {
+    int totalsubject;
+    float marks, total = 0, Average, percentage;
+
+    cout << "please enter the total number of subject = ";
+    cin >> totalsubject;
+
+    cout << "please enter the marks for each subject (out of 100) = ";
+    for (int i = 0; i < totalsubject; i++) {
+        cin >> marks;
+        total = total + marks;
+    }
+
+    Average = total / totalsubject;
+    percentage = (total / (totalsubject * 100)) * 100;
+
+    cout << "\ntotal marks obtained = " << total
+         << " out of " << totalsubject * 100 << endl;
+    cout << "Average marks = " << Average << endl;
+    cout << "marks percentage = " << percentage << "%" << endl;
+
+    return 0;
+}
