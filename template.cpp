98a5b4f10235923b7247a5f78ae3e03c0f65ef24
yashwardhan96 <yashwardhansawant969696@gmail.com>
2026-04-02 10:17:53 +0530

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+ template <typename T> T mymax(T x,T y){
+ return (x>y) ? x:y;
+ }
+ int main () {
+    cout<<mymax <int> (3,7) <<endl;
+    cout<<mymax <double> (3.0,4.0) <<endl;
+    cout<<mymax <char> ('g','e') <<endl;
+    return 0;
+ }
